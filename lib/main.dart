de925635a561c26396e53a0f313c92414b25b133
FlutLab.io <>
2021-10-08 11:48:31 +0000

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,64 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red[600],
+        appBar: AppBar(
+          backgroundColor: Colors.red[600],
+          title: Center(
+            child: Text('DICE'),
+          ),
+        ),
+        body: BodyPage(),
+      ),
+    ),
+  );
+}
+
+class BodyPage extends StatefulWidget {
+  @override
+  _BodyPageState createState() => _BodyPageState();
+}
+
+class _BodyPageState extends State<BodyPage> {
+  var diceNum1 = 1;
+  var diceNum2 = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(2.0),
+              child: FlatButton(
+                onPressed: () {
+                  setState(() {
+                    diceNum1 = Random().nextInt(6) + 1;
+                  });
+                },
+                child: Image.asset('images/dice$diceNum1.png'),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(2.0),
+              child: FlatButton(
+                onPressed: () {
+                  setState(() {
+                    diceNum2 = Random().nextInt(6) + 1;
+                  });
+                },
+                child: Image.asset('images/dice$diceNum2.png'),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
